a199489cade77a317de49bd1205e5d2ae2c32715
moise7000 <77933570+moise7000@users.noreply.github.com>
2025-10-13 17:37:34 +0200

    [+] New xml

--- app/src/main/java/com/example/projetamio/MainActivity.java
+++ app/src/main/java/com/example/projetamio/MainActivity.java
@@ -1,28 +1,33 @@
 package com.example.projetamio;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-        Log.d("MainActivity", "[HERE] Création de l'activité");
 
+        Log.d(TAG, "Création de l'activité");
 
+        // Démarrer le service
+        Intent serviceIntent = new Intent(this, MainService.class);
+        startService(serviceIntent);
+        Log.d(TAG, "Demande de démarrage du service");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Arrêter le service
+        Intent serviceIntent = new Intent(this, MainService.class);
+        stopService(serviceIntent);
+        Log.d(TAG, "Demande d'arrêt du service");
     }
 }
--- app/src/main/java/com/example/projetamio/MainService.java
+++ app/src/main/java/com/example/projetamio/MainService.java
@@ -8,31 +8,28 @@ import android.os.IBinder;
 
 
 public class MainService extends Service {
-
     private static final String TAG = "MainService";
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d(TAG, "Service créé - onCreate()");
+        Log.d(TAG, "Service créé");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "Service démarré - onStartCommand()");
-
-        return START_STICKY;
+        Log.d(TAG, "Service démarré");
+        return START_STICKY; // Mode sticky pour redémarrage automatique
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "Service détruit - onDestroy()");
-
+        Log.d(TAG, "Service détruit");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return null; // Service non lié
     }
 }
