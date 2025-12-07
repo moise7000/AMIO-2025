89170ab9647693ba80382a7fe4e7af1f86037a10
Leo <leo.gurtner@telecomnancy.eu>
2025-12-07 18:18:00 +0100

    Ligth's watch

---
+++ app/src/main/java/com/example/projetamio/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.projetamio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000; // 2 secondes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
